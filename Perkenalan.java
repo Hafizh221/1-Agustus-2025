543eaaf55e3b3d37f16de231d8950e16217a1600
HafizhRPL <hafizh_syarif_34rpl@student.smktelkom-mlg.sch.id>
2025-08-01 14:10:42 +0700

    Add files via upload

---
+++ Perkenalan.java
@@ -0,0 +1,10 @@
+public class Perkenalan {
+    public static void main(String[] args) {
+        System.out.println("Perkenalkan nama saya Hafizh Syahwan Al Syarif");
+        System.out.println("Asal sekolah saya SMPN 1 Lumajang");
+        System.out.println("Alamat rumah saya Lumajang, Rowokangkung tepatnya di jalan Mayjend Soekertijo");
+        System.out.println("Hobi saya adalah ermain Game dan lari");
+        System.out.println("Cita cita saya menjadi Front-End Developer");
+        System.out.println("Saya sekarang berusia 16 tahun");
+    }
+}
